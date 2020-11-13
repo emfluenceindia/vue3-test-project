61df4217dbda1191d36e02cf67324fbcc0e83ed6
Subrata Sarkar <ssarkar@emfluence.com>
2020-11-13 19:12:06 +0530

    Added image data in home.vue

--- src/pages/static/Home.vue
+++ src/pages/static/Home.vue
@@ -1,12 +1,73 @@
 <template>
-    <div>
-        <h1>Welcome to Aim High</h1>
+    <div class="w-full">
+        <section>
+            <h2 class="text-2xl p-2">Recently Added</h2>
+            <div class="flex flex-wrap mb-4 m-auto">
+                <div class="w-1/6 my-2" v-for="image in images" :key="image.id">
+                    <img class="rounded" :src="image.url" alt="Some text" />
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Home'
+    name: 'Home',
+    data() {
+        return {
+            images: [
+                {
+                    id: 1,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/1.jpg?thumbnail=200'
+                },
+                {
+                    id: 2,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/2.jpg?thumbnail=200'
+                },
+                {
+                    id: 3,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/3.jpg?thumbnail=200'
+                },
+                {
+                    id: 4,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/4.jpg?thumbnail=200'
+                },
+                {
+                    id: 5,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/5.jpg?thumbnail=200'
+                },
+                {
+                    id: 6,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/6.jpg?thumbnail=200'
+                },
+                {
+                    id: 7,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/8.jpg?thumbnail=200'
+                },
+                {
+                    id: 8,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/8.jpg?thumbnail=200'
+                },
+                {
+                    id: 9,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/9.jpg?thumbnail=200'
+                },
+                {
+                    id: 10,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/10.jpg?thumbnail=200'
+                },
+                {
+                    id: 11,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/11.jpg?thumbnail=200'
+                },
+                {
+                    id: 12,
+                    url: 'https://emfluenceindia.sirv.com/Sandakphu/12.jpg?thumbnail=200'
+                },
+            ]
+        }
+    }
 }
 </script>
 
