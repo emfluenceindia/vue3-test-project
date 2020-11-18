fe68499311a1cb7c5eeed8c464c2fcc9ac76fb56
Subrata Sarkar <ssarkar@emfluence.com>
2020-11-18 17:49:43 +0530

    Wrapped login controls inside<form> element
    Created a form data object and controls are set with v-model for 2-way binding.
    
    The method "submitLogin" called from submit.prevent event logs user enetered data properly into the console.

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -3,24 +3,26 @@
         <section class="z-20 w-screen h-screen bg-gray-400 fixed top-0 opacity-75" @click="showLoginModal(false)"></section>
         <div class="absolute inset-0">
             <div class="flex h-full">
-                <div class="z-30 m-auto py-3 px-5 w-4/12 bg-white rounded-md shadow-2xl">
-                    <fieldset class="border rounded border-gray-400 p-2">
-                        <legend class="text-left text-sm uppercase font-semibold px-1">Login</legend>
-                        <div class="w-11/12 m-auto">
-                            <div class="w-full text-left">
-                                <label class="block w-full text-sm font-medium" for="txtLoginId">Username</label>
-                                <input type="text" id="txtLoginId" required maxlength="75" class="border border-l-0 border-r-0 border-t-0 border-orange-400 w-full outline-none" />
+                <div class="z-30 m-auto p-3 w-5/12 bg-gray-200 rounded-md shadow-2xl">
+                    <fieldset class="border rounded border-white p-2 w-full bg-white shadow-xl">
+                        <legend class="text-left text-lg uppercase font-semibold px-2 rounded bg-white text-gray-600">Log in</legend>
+                        <form @submit.prevent="submitLogin">
+                            <div class="w-11/12 m-auto">
+                                <div class="w-full text-left mt-5">
+                                    <label class="block w-full text-sm font-medium" for="txtLoginId">Email or Username</label>
+                                    <input v-model="loginData.loginEmail" type="text" id="txtLoginId" required maxlength="75" class="border border-l-0 border-r-0 border-t-0 border-gray-300 w-full outline-none" />
+                                </div>
+                                <div class="w-full text-left mt-3 mb-2">
+                                    <label class="block w-full text-sm font-medium" for="txtLoginPassword">Password</label>
+                                    <input v-model="loginData.loginPassword" type="password" id="txtLoginPassword" required maxlength="30" class="border border-l-0 border-r-0 border-t-0 border-gray-300 w-full outline-none" />
+                                </div>
                             </div>
-                            <div class="w-full text-left mt-3 mb-2">
-                                <label class="block w-full text-sm font-medium" for="txtLoginPassword">Password</label>
-                                <input type="password" id="txtLoginPassword" required maxlength="30" class="border border-l-0 border-r-0 border-t-0 border-orange-400 w-full outline-none" />
+                            <div class="mt-5 pb-3">
+                                <button type="submit" class="bg-gradient-to-b from-green-500 to-green-700 rounded text-white py-1 px-3 text-xs antialiased tracking-wide uppercase mx-2 outline-none">submit</button>
+                                <button class="bg-gradient-to-b from-orange-500 to-orange-700 rounded text-white py-1 px-3 text-xs antialiased tracking-wide uppercase outline-none" @click="showLoginModal(false)">Cancel</button>
                             </div>
-                        </div>
+                        </form>
                     </fieldset>
-                    <div class="mt-5">
-                        <button class="bg-green-600 rounded text-white py-1 px-3 text-xs antialiased tracking-wide uppercase mx-2 outline-none">login</button>
-                        <button class="bg-orange-600 rounded text-white py-1 px-3 text-xs antialiased tracking-wide uppercase outline-none" @click="showLoginModal(false)">Cancel</button>
-                    </div>
                 </div>
             </div>
         </div>
@@ -39,16 +41,20 @@ export default {
     data() {
         return {
             isOpened: this.show,
+            loginData: {
+                loginEmail: '',
+                loginPassword: ''
+            }
         }
     },
     methods: {
         showLoginModal(value) {
             this.$emit('hidemodal', value);
+        },
+
+        submitLogin() {
+            console.log(this.loginData);
         }
     }
 }
 </script>
-
-<style scoped>
-
-</style>
