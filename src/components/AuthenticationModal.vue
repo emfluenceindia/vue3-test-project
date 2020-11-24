04a0960178306211a71b5c4e9d35e998d1b1b951
Subrata Sarkar <ssarkar@emfluence.com>
2020-11-24 18:12:20 +0530

    Removed overlay from Login modal. Only the form remains now.

--- src/components/AuthenticationModal.vue
+++ src/components/AuthenticationModal.vue
@@ -7,14 +7,16 @@
                 <div class="z-30 m-auto p-3 w-5/12 bg-gray-200 rounded-md shadow-2xl">
                     <fieldset class="border rounded border-white p-2 w-full bg-white shadow-xl">
                         <legend class="text-left text-lg uppercase font-semibold px-2 rounded bg-white text-gray-600">{{popupLabel}}</legend>
+                        
                         <!-- Login popup -->
                         <section v-if="popupId === 1">
                             <Login :show="true" />
                         </section>
+                        
+                        <!-- Register popup -->
                         <section v-else>
                             <Register :showPopup="true" />
                         </section>
-                        <!-- Register popup -->
                     </fieldset>
                 </div>
             </div>
@@ -23,14 +25,12 @@
 </template>
 
 <script>
-//import Button from './Button.vue';
 import Login from './LoginModal.vue';
 import Register from './RegisterModal.vue';
 
 export default {
     name: 'AuthicationModal',
     components: {
-        //Button,
         Login,
         Register
     },
--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -1,38 +1,28 @@
 <template>
     <section v-if="show">
-        <section class="z-20 w-screen h-screen bg-gray-400 fixed top-0 opacity-75" @click="showLoginModal(false)"></section>
-        <div class="absolute inset-0">
-            <div class="flex h-full">
-                <div class="z-30 m-auto p-3 w-5/12 bg-gray-200 rounded-md shadow-2xl">
-                    <fieldset class="border rounded border-white p-2 w-full bg-white shadow-xl">
-                        <legend class="text-left text-lg uppercase font-semibold px-2 rounded bg-white text-gray-600">Log in</legend>
-                        <form @submit.prevent="submitLogin">
-                            <div class="w-11/12 m-auto">
-                                <div class="w-full text-left mt-5">
-                                    <label class="block w-full text-sm font-medium" for="txtLoginId">Email or Username</label>
-                                    <input v-model="loginData.loginEmail" type="text" ref="loginInputRef" required maxlength="75" class="border border-l-0 border-r-0 border-t-0 border-gray-300 w-full outline-none" />
-                                </div>
-                                <div class="w-full text-left mt-3 mb-2">
-                                    <label class="block w-full text-sm font-medium" for="txtLoginPassword">Password</label>
-                                    <input v-model="loginData.loginPassword" type="password" ref="txtLoginPassword" id="txtLoginPassword" required maxlength="30" class="border border-l-0 border-r-0 border-t-0 border-gray-300 w-full outline-none" />
-                                </div>
-                            </div>
-                            <div class="mt-5 pb-3">
-                                <Button behavior="submit" baseColor="green">
-                                    <span><i class="fa fa-sign-in" aria-hidden="true"></i>&nbsp;sign in</span>
-                                </Button>
-                                <Button behavior="submit" baseColor="orange" @click.prevent="showLoginModal(false)">
-                                    <span><i class="fa fa-ban" aria-hidden="true"></i>&nbsp;cancel</span>
-                                </Button>
-                            </div>
-                            <div class="mt-5 pb-3">
-                                <a href="" @click.prevent="closeLoginAndOpenRegister(false)">Create a free Account!</a>
-                            </div>
-                        </form>
-                    </fieldset>
+        <form @submit.prevent="submitLogin">
+            <div class="w-11/12 m-auto">
+                <div class="w-full text-left mt-5">
+                    <label class="block w-full text-sm font-medium" for="txtLoginId">Email or Username</label>
+                    <input v-model="loginData.loginEmail" type="text" ref="loginInputRef" required maxlength="75" class="border border-l-0 border-r-0 border-t-0 border-gray-300 w-full outline-none" />
+                </div>
+                <div class="w-full text-left mt-3 mb-2">
+                    <label class="block w-full text-sm font-medium" for="txtLoginPassword">Password</label>
+                    <input v-model="loginData.loginPassword" type="password" ref="txtLoginPassword" id="txtLoginPassword" required maxlength="30" class="border border-l-0 border-r-0 border-t-0 border-gray-300 w-full outline-none" />
                 </div>
             </div>
-        </div>
+            <div class="mt-5 pb-3">
+                <Button behavior="submit" baseColor="green">
+                    <span><i class="fa fa-sign-in" aria-hidden="true"></i>&nbsp;sign in</span>
+                </Button>
+                <Button behavior="submit" baseColor="orange" @click.prevent="showLoginModal(false)">
+                    <span><i class="fa fa-ban" aria-hidden="true"></i>&nbsp;cancel</span>
+                </Button>
+            </div>
+            <div class="mt-5 pb-3">
+                <a href="" @click.prevent="closeLoginAndOpenRegister(false)">Create a free Account!</a>
+            </div>
+        </form>
     </section>
 </template>
 
