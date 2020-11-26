9ef013531ad12a54c8cab906fdf139cb64265d00
Subrata Sarkar <ssarkar@emfluence.com>
2020-11-26 13:39:10 +0530

    Created new Navigation component
    The reason for creating a separate Navigation component is to reuse the it in both header and footer. Layout would be different based on the the value of the "position" prop. Default value is "header"

--- src/components/InfoTrekHeader.vue
+++ src/components/InfoTrekHeader.vue
@@ -32,24 +32,17 @@
         </div>
 
         <!-- mavigation -->
-        <nav class="bg-gradient-to-l from-green-600 to-green-400 p-2 mt-6 box-border align-baseline w-full border-b-2 border-gray-400">
-            <ul class="flex justify-left">
-                <li class="mx-1 box-border" v-for="menuItem in menuItems" :key="menuItem.id">
-                    <router-link :class="menuItem.title.toLowerCase()" class="tracking-wide px-3 py-1 uppercase text-sm block font-semibold text-green-900 hover:shadow" :to="menuItem.link">{{menuItem.title}}</router-link>
-                </li>
-                <li class="mx-1 box-border">
-                    <a class="tracking-wide cursor-pointer px-3 py-1 uppercase text-sm block font-semibold text-green-900 hover:shadow" v-if="isAuthenticated" @click.prevent="signOut">Logout</a>
-                    <a v-else class="tracking-wide cursor-pointer px-3 py-1 uppercase text-sm block font-semibold text-green-900 hover:shadow" @click.prevent="openModal(showModal, 'login', 1)">Login</a>
-                </li>
-            </ul>
-        </nav>
-        
+        <Navigation :isAuthenticated="isAuthenticated" :position="header" />
     </header>
 </template>
 
 <script>
+import Navigation from './MainNavigation.vue';
 export default {
     name: 'InfoTrekHeader',
+    components: {
+        Navigation
+    },
     props: {
         brand: {
             type: String,
@@ -101,33 +94,7 @@ export default {
                 }
             ],
 
-            menuItems: [
-                {
-                    id: 1,
-                    title: 'Home',
-                    link: '/'
-                },
-                {
-                    id: 2,
-                    title: 'Destinations',
-                    link: '/destinations'
-                },
-                {
-                    id: 3,
-                    title: 'Stories',
-                    link: '/stories'
-                },
-                {
-                    id: 4,
-                    title: 'About',
-                    link: '/about'
-                },
-                {
-                    id: 5,
-                    title: 'Contact',
-                    link: '/contact'
-                }
-            ],
+            
 
             isLoggedIn: true,
             showModal: true,
